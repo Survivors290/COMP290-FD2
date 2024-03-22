7c8505c5a266bf67c4d35c85b1aea0c2ee84453c
Gabriel <arnolgab@dickinson.edu>
2024-03-22 10:55:21 -0400

    check crop dropdown options in E2E sub-tab using Cypress

--- farmdata2/farmdata2_modules/fd2_school/e2e/e2e.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/e2e/e2e.defaults.spec.js
@@ -15,4 +15,15 @@ describe("Test the harvest report default values", () => {
         cy.get("[data-cy=end-date]")
             .should("have.value", "2020-05-15")
     })
+
+    it("Check crop selection values", () => {
+        cy.get("[data-cy=crop-selection]").children().eq(0)
+            .should("have.value", "ARUGULA")
+        cy.get("[data-cy=crop-selection]").children().eq(4)
+            .should("have.value", "BEAN-FAVA")
+        cy.get("[data-cy=crop-selection]").children().eq(110)
+            .should("have.value", "ZUCCHINI")
+        cy.get("[data-cy=crop-selection]").children()
+            .should("have.length", "111")
+    })
 })
