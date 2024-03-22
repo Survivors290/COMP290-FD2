efa3c5ef984490c95989058914cbfb59c9adf924
MuellerrAtDickinsonDotedu <muellerr@dickinson.edu>
2024-03-22 15:50:27 -0400

    Added a test for checking that the crop selection is correct

--- farmdata2/farmdata2_modules/fd2_school/e2e/e2e.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/e2e/e2e.defaults.spec.js
@@ -13,4 +13,11 @@ describe('Test the harvest report default values', () => {
     cy.get("[data-cy=start-day]").should("have.value","2020-05-05")
     cy.get("[data-cy=end-day]").should("have.value","2020-05-15")
   })
+
+  it("Check that the crop selection is correct", () => {
+    cy.get("[data-cy=select-crop]").children().eq(0).should("have.text","ARUGULA")
+    cy.get("[data-cy=select-crop]").children().eq(4).should("have.text","BEAN-FAVA")
+    cy.get("[data-cy=select-crop]").children().eq(111).should("have.text","ZUCCHINI")
+    cy.get("[data-cy=select-crop]").children().should("have.length","112")
+  })
 })
