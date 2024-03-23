88d376835b11896b7b3ff4d4a7156ea53c191349
SpencerGoodman <goodmasp@dickinson.edu>
2024-03-23 16:05:53 -0400

    First cypress test for farmdata added

--- farmdata2/farmdata2_modules/fd2_school/first.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/first.spec.js
@@ -1,7 +1,10 @@
-describe('My First Test', () => {
-  it('clicks the link "type"', () => {
-    cy.visit('https://example.cypress.io')
-
-    cy.contains('type').click()
+describe("Test the harvest report default values", () => {
+  beforeEach(() => {
+    cy.login("manager1", "farmdata2")
+    cy.visit("/farm/fd2-school/e2e")
+  })
+  it("Check the page header", () => {
+    cy.get("[data-cy=page-header]")
+      .should("have.text","Harvest Report")
   })
 })
