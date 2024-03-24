7cf86fa19e887097c7386b7d4339667cfeb63ca2
shaikhibrahim2000 <ahnafrahman305@gmail.com>
2024-03-24 04:02:21 -0400

    Added E2E test for checking the report details

--- farmdata2/farmdata2_modules/fd2_school/e2e/e2e.dynamic.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/e2e/e2e.dynamic.spec.js
@@ -10,4 +10,17 @@ describe("Test the harvest report default dynamic values", () => {
         cy.get("[data-cy=report-title]").should("be.visible")
        
     })
+
+    it("Check report details", () => {
+        cy.get("[data-cy=farm-name]").should("not.exist")
+        cy.get("[data-cy=language]").should("not.exist")
+        cy.get("[data-cy=user]").should("not.exist")
+
+        cy.get("[data-cy=generate-report]").click()
+
+        cy.get("[data-cy=farm-name]").should("have.text","Farm:  Sample Farm")
+        cy.get("[data-cy=language]").should("have.text","en")
+        cy.get("[data-cy=user]").should("have.text","manager1")
+       
+    })
 })
