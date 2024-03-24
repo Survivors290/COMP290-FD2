dccd3ae9cbb8a51fb7fb74a2925d97cf957e77ba
MuellerrAtDickinsonDotedu <muellerr@dickinson.edu>
2024-03-24 15:22:35 -0400

    created a test to make sure that the farm, user, and language data fields in report were correct. Added data-cy tags to relevent html elements in e2e.html

--- farmdata2/farmdata2_modules/fd2_school/e2e/e2e.interactions.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/e2e/e2e.interactions.spec.js
@@ -4,10 +4,18 @@ describe("hi", () => {
         cy.visit("/farm/fd2-school/e2e")
     })
 
-    it("Click the generate report button", () => {
+    it("Click the generate report button, check visibility", () => {
         cy.get("[data-cy=report-header").should("not.exist")
         cy.get("[data-cy=generate-button]").click()
         cy.get("[data-cy=report-header]").should("be.visible")
+    })
+
+    it("Check report elements after generate report", () => {
+        cy.get("[data-cy=generate-button]").click()
+        cy.get("[data-cy=report-farm]").should("has.text", "Farm: Sample Farm")
+        cy.get("[data-cy=report-user]").should("contain.text", "manager1")
+        cy.get("[data-cy=report-language]").should("have.text", "English")
+
     })
 
 })
