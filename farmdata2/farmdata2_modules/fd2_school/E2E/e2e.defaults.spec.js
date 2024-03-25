d29dfa9ccc7fc453c3665eb55148ea2a9aa90cd7
cschnydman <schnydmc@dickinson.edu>
2024-03-24 21:34:18 -0400

    Added test for the crop dropdown in Harvest Report Crop List Spike 3

--- farmdata2/farmdata2_modules/fd2_school/E2E/e2e.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/E2E/e2e.defaults.spec.js
@@ -10,5 +10,11 @@ it('Check the default start and end dates', ()=> {
   cy.get("[data-cy=start-date]").should("have.value", "2020-05-05")
   cy.get("[data-cy=end-date]").should("have.value", "2020-05-15")
 })
+it('Check the crops in the crop dropdown', ()=> {
+  cy.get("[data-cy=crop-dropdown]").children().eq(0).should("have.text", "ARUGULA")
+  cy.get("[data-cy=crop-dropdown]").children().eq(4).should("have.text", "BEAN-FAVA")
+  cy.get("[data-cy=crop-dropdown]").children().eq(110).should("have.text", "ZUCCHINI")
+  cy.get("[data-cy=crop-dropdown]").children().should("have.length", "111")
+})
 
 })
