e2a42bb5ffb913352dfc9aad70a1c634e39d71a2
megantriplett <megantriplett10@gmail.com>
2024-03-28 23:18:09 -0400

    Added Cypress tests for harvest table headers

--- farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
@@ -4,8 +4,13 @@ describe("Test the harvest report table", () => {
         cy.visit("/farm/fd2-school/fd2")
     })
       
-    it("", () => {
-        
+    it("Checks header values", () => {
+        cy.get('[data-cy=generate-report-button]').click()
+        cy.get("[data-cy=h0]").should("have.text","Date")
+        cy.get("[data-cy=h1]").should("have.text","Area")
+        cy.get("[data-cy=h2]").should("have.text","Crop")
+        cy.get("[data-cy=h3]").should("have.text","Yield")
+        cy.get("[data-cy=h4]").should("have.text","Units")
     })
     
 })
