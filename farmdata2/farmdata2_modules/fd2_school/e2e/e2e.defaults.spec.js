b2e2fa15619678854e4e130a4364d625ebfd24e7
fredam2025 <fredam@dickinson.edu>
2024-03-29 11:25:38 -0400

    Added crop drop down cypress tests

--- farmdata2/farmdata2_modules/fd2_school/e2e/e2e.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/e2e/e2e.defaults.spec.js
@@ -8,10 +8,20 @@ describe("Test the harvest report default values", () => {
         cy.get("[data-cy=page-header]")
             .should("have.text", "Harvest Report")
     })
-    it("Check the ", () => {
+    it("Check the start and end dates", () => {
         cy.get("[data-cy=start-date]")
             .should("have.value", "2020-05-05")
         cy.get("[data-cy=end-date]")
             .should("have.value", "2020-05-15")    
+    })
+    it("Check the crop drop down list", () => {
+        cy.get("[data-cy=cropDropdown]").children().eq(0)
+            .should("have.value", "ARUGULA")  
+        cy.get("[data-cy=cropDropdown]").children().eq(4)
+            .should("have.value", "BEAN-FAVA")  
+        cy.get("[data-cy=cropDropdown]").children().eq(110)
+            .should("have.value", "ZUCCHINI")  
+        cy.get("[data-cy=cropDropdown]").children()
+            .should("have.length", 111)   
     })
 })
