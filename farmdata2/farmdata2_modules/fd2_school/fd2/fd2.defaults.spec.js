ba8469fe01a0c5e451bed9f0679ed12190f70d88
MuellerrAtDickinsonDotedu <muellerr@dickinson.edu>
2024-03-29 12:10:03 -0400

    created fd2 directory, coppied over e2e directory, and added fd2 subtab to the fd2_school.module

---
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.defaults.spec.js
@@ -0,0 +1,23 @@
+describe('Test the harvest report default values', () => {
+    beforeEach(() => {
+      cy.login("manager1","farmdata2")
+      cy.visit("/farm/fd2-school/e2e")
+    })
+  
+    it("Check the page header", () => {
+      cy.get("[data-cy=page-header]")
+        .should("have.text","Harvest Report")
+    })
+  
+    it("Check the beginning and end dates", () => {
+      cy.get("[data-cy=start-day]").should("have.value","2020-05-05")
+      cy.get("[data-cy=end-day]").should("have.value","2020-05-15")
+    })
+  
+    it("Check that the crop selection is correct", () => {
+      cy.get("[data-cy=select-crop]").children().eq(0).should("have.text","ARUGULA")
+      cy.get("[data-cy=select-crop]").children().eq(4).should("have.text","BEAN-FAVA")
+      cy.get("[data-cy=select-crop]").children().eq(111).should("have.text","ZUCCHINI")
+      cy.get("[data-cy=select-crop]").children().should("have.length","112")
+    })
+  })
