45d6248999cc0f1cb1241e34cb5561d436d746e3
fredam2025 <fredam@dickinson.edu>
2024-03-29 11:12:14 -0400

    Added start and end date cypress tests

--- farmdata2/farmdata2_modules/fd2_school/e2e/e2e.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/e2e/e2e.defaults.spec.js
@@ -8,4 +8,10 @@ describe("Test the harvest report default values", () => {
         cy.get("[data-cy=page-header]")
             .should("have.text", "Harvest Report")
     })
+    it("Check the ", () => {
+        cy.get("[data-cy=start-date]")
+            .should("have.value", "2020-05-05")
+        cy.get("[data-cy=end-date]")
+            .should("have.value", "2020-05-15")    
+    })
 })
