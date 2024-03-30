62d3f9a6a1cd33b101fba30a0e18b6ffdf7d201d
MuellerrAtDickinsonDotedu <muellerr@dickinson.edu>
2024-03-30 18:43:50 -0400

    made a more vigurouse test for correct header titles

--- farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
@@ -6,7 +6,16 @@ describe("test report table", () => {
     it("click generate report button, and check headers", () => {
         cy.get("[data-cy=generate-button]").click()
         cy.get("[data-cy=report-table]")
-        cy.get("[data-cy=table-headers]").children().should("has.text", "IDDateAreaCropYieldUnits")
+        cy.get("[data-cy=h0]").should("has.text", "ID")
+        cy.get("[data-cy=h1]").should("has.text", "Date")
+        cy.get("[data-cy=h2]").should("has.text", "Area")
+        cy.get("[data-cy=h3]").should("has.text", "Crop")
+        cy.get("[data-cy=h4]").should("has.text", "Yield")
+        cy.get("[data-cy=h5]").should("has.text", "Units")
         cy.get("[data-cy=table-headers]").children().should("have.length","7")
+    })
+    it("Check table values when changing crop selection", () => {
+        cy.get("[data-cy=generate-button]").click()
+
     })
 })
