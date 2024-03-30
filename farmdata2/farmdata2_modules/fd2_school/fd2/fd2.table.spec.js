feb0dea7fe217341ede8c00c3c8bc092fc805acd
MuellerrAtDickinsonDotedu <muellerr@dickinson.edu>
2024-03-30 18:29:51 -0400

    added test for correct table headers

--- farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
@@ -1,9 +1,11 @@
 describe("test report table", () => {
     beforeEach(() => {
         cy.login("manager1","farmdata2")
-        cy.visit("/farm/fd2-school/e2e")
+        cy.visit("/farm/fd2-school/fd2")
     })
-    it("empty", () => {
-        
+    it("click generate report button, and check headers", () => {
+        cy.get("[data-cy=generate-button]").click()
+        cy.get("[data-cy=report-table]")
+        cy.get("[data-cy=table-headers]").children(0).should("has.text", "IDDateAreaCropYieldUnits")
     })
 })
