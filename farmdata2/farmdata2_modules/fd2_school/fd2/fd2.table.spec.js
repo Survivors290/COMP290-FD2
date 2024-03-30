b267ca80c5f4a5a51824e0e298f3efa4d4ed8fe3
MuellerrAtDickinsonDotedu <muellerr@dickinson.edu>
2024-03-30 18:10:32 -0400

    created test for report table, nothing currently tested

---
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
@@ -0,0 +1,9 @@
+describe("test report table", () => {
+    beforeEach(() => {
+        cy.login("manager1","farmdata2")
+        cy.visit("/farm/fd2-school/e2e")
+    })
+    it("empty", () => {
+        
+    })
+})
