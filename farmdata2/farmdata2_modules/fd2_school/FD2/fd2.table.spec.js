57aa6e6d7bffff113062fb30150469a937a2eab3
cschnydman <schnydmc@dickinson.edu>
2024-03-31 16:24:25 -0400

    Edited fd2.table.spec.js to test all the table headers

--- farmdata2/farmdata2_modules/fd2_school/FD2/fd2.table.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/FD2/fd2.table.spec.js
@@ -1,9 +1,15 @@
-describe('Test the harvest report dynamic features', () => {
+describe('Test the harvest report custom table', () => {
     beforeEach(() => {
         cy.login('manager1', 'farmdata2')
         cy.visit('/farm/fd2-school/fd2')
     })
-    it('Check custom table', ()=> {
+    it('Check custom table headers', ()=> {
+        cy.get("[data-cy=generate-report-button]").click()
+        cy.get("[data-cy=h0]").should("have.text","Date")
+        cy.get("[data-cy=h1]").should("have.text","Area")
+        cy.get("[data-cy=h2]").should("have.text","Crop")
+        cy.get("[data-cy=h3]").should("have.text","Yield")
+        cy.get("[data-cy=h4]").should("have.text","Units")
 
     })
 
