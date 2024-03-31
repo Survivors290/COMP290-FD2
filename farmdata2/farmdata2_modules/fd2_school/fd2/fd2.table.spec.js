4cc6faa93a69842d7b4245e75293ad79b917aebf
SpencerGoodman <goodmasp@dickinson.edu>
2024-03-31 18:21:32 -0400

    Tables headers test added

--- farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
@@ -3,6 +3,22 @@ describe("Test the harvest report default values", () => {
       cy.login("manager1", "farmdata2")
       cy.visit("/farm/fd2-school/fd2")
     })
-    it("Check crop dropdown", () => {
+    it("Click table", () => {
+        cy.get("[data-cy=generate-report-button]").click()
+    })
+    it("Check table headers", () => {
+        cy.get("[data-cy=generate-report-button]").click()
+        cy.get("[data-cy=h0]")
+        .should("have.text","Row")
+        cy.get("[data-cy=h1]")
+        .should("have.text","Date")
+        cy.get("[data-cy=h2]")
+        .should("have.text","Area")
+        cy.get("[data-cy=h3]")
+        .should("have.text","Crop")
+        cy.get("[data-cy=h4]")
+        .should("have.text","Yield")
+        cy.get("[data-cy=h5]")
+        .should("have.text","Units")
     })
   })
