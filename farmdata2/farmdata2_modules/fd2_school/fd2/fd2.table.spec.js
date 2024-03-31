55d9c16f82d7f4e5975c2706deb7bd8eb1c48402
Gabriel <arnolgab@dickinson.edu>
2024-03-31 17:06:00 -0400

    use Cypress testing to check for number of table headers in FD2 sub-tab

--- farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
@@ -4,10 +4,6 @@ describe("Test the harvest report table", () => {
         cy.visit("/farm/fd2-school/fd2")
     })
 
-    it("Click 'Generate Report' button", () => {
-        cy.get("[data-cy=generate-report-button]").click()
-    })
-      
     it("Check table headers", () => {
         cy.get("[data-cy=generate-report-button]").click()
         cy.get("[data-cy=h0]").should("have.text", "Row")
@@ -16,5 +12,10 @@ describe("Test the harvest report table", () => {
         cy.get("[data-cy=h3]").should("have.text", "Crop")
         cy.get("[data-cy=h4]").should("have.text", "Yield")
         cy.get("[data-cy=h5]").should("have.text", "Units")
+    })
+
+    it("Check number of table headers", () => {
+        cy.get("[data-cy=generate-report-button]").click()
+        cy.get("[data-cy=table-headers]").children().should("have.length", "6")
     })
 })
