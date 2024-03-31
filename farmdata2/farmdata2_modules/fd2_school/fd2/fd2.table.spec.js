814e0445b0befe9cdb2d39dac6b1a8be64beb5a5
SpencerGoodman <goodmasp@dickinson.edu>
2024-03-31 18:14:44 -0400

    fd2.table.spec.js added

---
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
@@ -0,0 +1,8 @@
+describe("Test the harvest report default values", () => {
+    beforeEach(() => {
+      cy.login("manager1", "farmdata2")
+      cy.visit("/farm/fd2-school/fd2")
+    })
+    it("Check crop dropdown", () => {
+    })
+  })
