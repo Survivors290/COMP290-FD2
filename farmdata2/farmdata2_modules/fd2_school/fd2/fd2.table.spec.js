1a9c35f7e2be2141197fff551f22111de05dbc7b
Gabriel <arnolgab@dickinson.edu>
2024-03-31 16:59:56 -0400

    use Cypress testing to check if all table headers are correct

--- farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
@@ -3,8 +3,18 @@ describe("Test the harvest report table", () => {
         cy.login("manager1", "farmdata2")
         cy.visit("/farm/fd2-school/fd2")
     })
+
+    it("Click 'Generate Report' button", () => {
+        cy.get("[data-cy=generate-report-button]").click()
+    })
       
-    it("", () => {
-        
+    it("Check table headers", () => {
+        cy.get("[data-cy=generate-report-button]").click()
+        cy.get("[data-cy=h0]").should("have.text", "Row")
+        cy.get("[data-cy=h1]").should("have.text", "Date")
+        cy.get("[data-cy=h2]").should("have.text", "Area")
+        cy.get("[data-cy=h3]").should("have.text", "Crop")
+        cy.get("[data-cy=h4]").should("have.text", "Yield")
+        cy.get("[data-cy=h5]").should("have.text", "Units")
     })
 })
