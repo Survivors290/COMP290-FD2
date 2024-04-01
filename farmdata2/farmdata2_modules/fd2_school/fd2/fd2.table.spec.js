8bdff1b3e46c7f6543f3314d11500b61376ea17a
MaxiSanc37 <maxijsanchez37@gmail.com>
2024-03-31 23:33:03 -0400

    Test file fd2.table.spec.js now asserts that the headers have the correct text

--- farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
@@ -4,7 +4,13 @@ describe("Check the contents of the harvest report table", () => {
         cy.visit("/farm/fd2-school/fd2")
     })
       
-    it("", () => {
-        
+    it("Checks the table's headers", () => {
+        cy.get("[data-cy=generate-report-button]").click()
+        cy.get("[data-cy=h0]").should("have.text", "ID")
+        cy.get("[data-cy=h1]").should("have.text", "Date")
+        cy.get("[data-cy=h2]").should("have.text", "Area")
+        cy.get("[data-cy=h3]").should("have.text", "Crop")
+        cy.get("[data-cy=h4]").should("have.text", "Yield")
+        cy.get("[data-cy=h5]").should("have.text", "Units")
     })
 })
