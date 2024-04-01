593bf6053ea2133b444f1cff07c497bc8523eb29
fredam2025 <fredam@dickinson.edu>
2024-03-31 22:41:25 -0400

    Added new cypress tests for custom table crop filtering

---
+++ farmdata2/farmdata2_modules/fd2_school/FD2/fd2.table.spec.js
@@ -0,0 +1,33 @@
+describe("Test the custom table component", () => {
+    beforeEach (() => {
+        cy.login("manager1", "farmdata2")
+        cy.visit("/farm/fd2-school/fd2")
+    })
+
+    it("Check the page header", () => {
+        cy.get("[data-cy=generateButton]").click()
+        cy.get("[data-cy=h0]")
+            .should("have.text", "Row")
+        cy.get("[data-cy=h1]")
+            .should("have.text", "Date")
+        cy.get("[data-cy=h2]")
+            .should("have.text", "Area")
+        cy.get("[data-cy=h3]")
+            .should("have.text", "Crop")
+        cy.get("[data-cy=h4]")
+            .should("have.text", "Yield")
+        cy.get("[data-cy=h5]")
+            .should("have.text", "Units")
+        cy.get("[data-cy=test-table] > [data-cy=table-headers]").children()
+            .should("have.length", 6)  
+    })
+    it("Check if filtering by crop works correctly", () => {
+        cy.get("[data-cy=generateButton]").click()
+        cy.get("[data-cy=cropDropdown] > [data-cy=dropdown-input] > [data-cy=option15]")
+            .should("have.value", "CARROT")  
+        cy.get("[data-cy=test-table] > [data-cy=table]").children()
+            .should("have.length", 6)
+        cy.get("[data-cy=test-table] > [data-cy=table-headers] > [data-cy=h4")
+            .should("have.value", "CARROT")  
+    })
+})
