07a5581759eae497cd923db758fc3a61c40aa60a
MaxiSanc37 <maxijsanchez37@gmail.com>
2024-03-31 23:26:59 -0400

    Added test file fd2.table.spec.js to fd2 directory

---
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
@@ -0,0 +1,10 @@
+describe("Check the contents of the harvest report table", () => {
+    beforeEach(() => {
+        cy.login("manager1", "farmdata2")
+        cy.visit("/farm/fd2-school/fd2")
+    })
+      
+    it("", () => {
+        
+    })
+})
