4d25b7ddfb12ff56bb5d9a86d60622543ee19497
longbui23 <blong5791@gmai.com>
2024-04-02 00:51:56 -0400

    FD2: Check header of table

--- farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
@@ -1,5 +1,20 @@
 describe("Test the harvest report default values", () => {
   beforeEach(() => {
-    
+    cy.login("manager1", "farmdata2")
+    cy.visit("/farm/fd2-school/fd2")
   })
+
+  ("Test table report headers", () => {
+    cy.get("[data-cy=generate-btn]").click()
+    cy.get("[data-cy=h0]")
+    .should("have.text", "Date")
+    cy.get("[data-cy=h1]")
+    .should("have.text", "Area")
+    cy.get("[data-cy=h2]")
+    .should("have.text", "Crop")
+    cy.get("[data-cy=h3]")
+    .should("have.text", "Yield")
+    cy.get("[data-cy=h4]")
+    .should("have.text", "Units")
+})
 })
