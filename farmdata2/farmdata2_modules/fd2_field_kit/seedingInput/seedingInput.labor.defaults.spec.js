7f54ba1e43b9d385473f2969a5d33cf2d80f64c0
megantriplett <megantriplett10@gmail.com>
2024-04-09 13:00:15 -0400

    added Cypress test doc for Labor input

---
+++ farmdata2/farmdata2_modules/fd2_field_kit/seedingInput/seedingInput.labor.defaults.spec.js
@@ -0,0 +1,10 @@
+describe("Testing default inputs for Labor chart", () {
+    beforeEach(() => {
+        cy.login("manager1", "farmdata2")
+        cy.visit("/farm/fd2-field-kit/seedingInput")
+    })
+    
+    it("Checks header values", () => {
+        
+    })
+} )
