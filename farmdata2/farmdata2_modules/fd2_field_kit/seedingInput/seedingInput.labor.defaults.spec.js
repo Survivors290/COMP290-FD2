bc851c64b406e977da485d48c1659011f49fd83b
cschnydman <schnydmc@dickinson.edu>
2024-04-10 22:23:46 -0400

    Added Cypress tests for the crop-selection dropdown and the time unit dropdown. Tests check default states for both dropdowns

--- farmdata2/farmdata2_modules/fd2_field_kit/seedingInput/seedingInput.labor.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_field_kit/seedingInput/seedingInput.labor.defaults.spec.js
@@ -13,7 +13,10 @@ describe("Testing default inputs for Labor chart", () => {
     })
 
     it("Checks dropdown values are default", () => { //chloe
-        
+        //crop dropdown
+        cy.get("[data-cy=crop-selection]").children().eq(0).should("have.text", "Crop:*") 
+        // time unit dropdown
+        cy.get("[data-cy=time-unit] > [data-cy=dropdown-input]").children().eq(0).should("have.text", "minutes")
     })
 
     it("Checks default time unit", () => { //megan
