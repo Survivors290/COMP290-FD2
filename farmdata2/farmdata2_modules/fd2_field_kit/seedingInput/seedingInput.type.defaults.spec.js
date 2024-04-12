96c620231791eb1bc9baf1c694c9c7316d971927
longbui23 <builo@dickinson.edu>
2024-04-12 02:35:18 -0400

    Ticket 22 Resolved: Test Seeding Input Type Defaults

---
+++ farmdata2/farmdata2_modules/fd2_field_kit/seedingInput/seedingInput.type.defaults.spec.js
@@ -0,0 +1,50 @@
+describe("Test Seeding Input Default Type", () => {
+    beforeEach(() => {
+        cy.login("manager1","farmdata2");
+        cy.visit("/farm/fd2-field-kit/seedingInput");
+
+    });
+
+    /** Check elements for "Tray" is enabled. 
+     * binding with submitInProgress Value
+    */
+    it("1) Tray seeding is enabled", () => {
+        cy.get("[data-cy='tray-seedings']").should('be.enabled')
+    })
+    /** Check elements for "Direct" os enabled. 
+     * binding with submitInProgress Value
+    */
+    it("2) Direct seeding is enabled", () => {
+        cy.get("[data-cy='direct-seedings']").should('be.enabled')
+    })
+    
+    /** Check neither Tray nor Direct is selected */
+    it("3) Tray and Direct elements not selected", () => {
+        cy.get("[data-cy='tray-seedings']").should('not.be.checked')
+        cy.get("[data-cy='direct-seedings']").should('not.be.checked')
+    })
+
+    /** Check neither Tray nor Direct is selected */
+    it("4) message visible when tray and direct not selected", () => {
+        cy.get("[data-cy='tray-seedings']").should('not.be.checked')
+        cy.get("[data-cy='direct-seedings']").should('not.be.checked')
+        cy.contains("Please Select Tray Seeding or Direct Seeding")
+    })
+
+      /** Check the form elements for either the "Tray" and 
+    "Direct" seedings are not visible or do not exist. */
+
+    it("Check form elements of Tray is not visible or not exist", () => { 
+        cy.get("[data-cy=tray-area-selection]").should("not.be.visible")
+        cy.get("[data-cy=num-cell-input]").should("not.be.visible")
+        cy.get("[data-cy=num-tray-input]").should("not.be.visible")
+        cy.get("[data-cy=num-seed-input]").should("not.be.visible")
+    }) 
+    it("Check form elements of Direct is not visible or not exist", () => { 
+        cy.get("[data-cy=direct-area-selection]").should("not.be.visible")
+        cy.get("[data-cy=num-rowbed-input]").should("not.be.visible")
+        cy.get("[data-cy=unit-feet]").should("not.be.visible")
+        cy.get("[data-cy=num-feet-input]").should("not.be.visible")
+    }) 
+
+});
