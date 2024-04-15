f454e24c9067dd876467a7d516af63d489b09baf
Cubin1215 <Pranavazad1215@gmail.com>
2024-04-15 15:44:19 -0400

    Added attributes to the test of start and end date

--- farmdata2/farmdata2_modules/fd2_school/e2e/e2e.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/e2e/e2e.defaults.spec.js
@@ -3,9 +3,14 @@ describe("Test the harvest report default values", () => {
         cy.login("manager1", "farmdata2")
         cy.visit("/farm/fd2-school/e2e")
         cy.get("[data-cy=page-header]").should("have.text","Harvest Report")
+        cy.get("[data-cy=start-date]").should("have.value","2020-05-05")
+        cy.get("[data-cy=end-date").should("have.value","2020-05-15")
     })
       
     it("Check the page header", () => {
     
+    })
+    it("Check the default start and end date", () => {
+
     })
 })
