e877365624a29daa8c7a1860c584f78b04f0347a
Cubin1215 <Pranavazad1215@gmail.com>
2024-04-18 13:41:54 -0400

    Incorporated new assertions to test farm anme username and language

--- farmdata2/farmdata2_modules/fd2_school/e2e/e2e.user.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/e2e/e2e.user.spec.js
@@ -5,6 +5,9 @@ describe("Test the harvest report user output values", () => {
         cy.get("[data-cy=header-check]").should("not.exist")
         cy.get("[data-cy=generate-button]").click()
         cy.get("[data-cy=header-check]").should("be.visible");
+        cy.get("[data-cy=farm-name]").should("have.text", "Farm: Sample Farm");
+        cy.get("[data-cy=username]").should("contain.text", "manager1")
+        cy.get("[data-cy=language]").should("have.text", "English");
 
     })
     it("Check the generate report button", () => {
