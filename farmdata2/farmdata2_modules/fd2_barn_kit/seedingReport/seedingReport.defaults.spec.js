ff6d54b661cd9c0841711e31b353a633f158bd69
jadoont <tayyabajadoon11@gmail.com>
2024-05-11 18:13:38 -0400

    testing last 3, start dates, end dates and report visibility

--- farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReport.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReport.defaults.spec.js
@@ -1,24 +1,40 @@
+// This file tests the default contents of the Seeding Report page in the Barn
+const dayjs = require("dayjs")
 describe('"Seeding Report Default Content and Functionality', () => {
 
     beforeEach(() => {
         cy.login('manager1', 'farmdata2')
+        cy.restoreLocalStorage();
         cy.visit('/farm/fd2-barn-kit/seedingReport')
-        
+    })
+
+    it("Check the page header", () => {
         cy.get("[data-cy=page-header]").should("have.text","Seeding Report")
-        cy.get("[data-cy=set-dates-label]").should("exist")
+    })
+   
+    it("Check the Generate Report button", () => {
         cy.get("[data-cy=generate-rpt-btn]").should("have.text","Generate Report")
         cy.get("[data-cy=generate-rpt-btn]").should("not.be.disabled")
     })
 
-    it("Check the page header", () => {
-   
-    })
     it("Check the Set Dates label", () => {
+        cy.get("[data-cy=set-dates-label]").should("exist")
+    })
+    
+
+    it("Check the start date", () => {
+        cy.get('[data-cy=start-date-select]>[data-cy=date-select]').should("have.value", "2024-01-01")
 
     })
-    it("Check the Generate Report button", () => {
+        it("Check the end date", () => {
+            let today = dayjs().format("YYYY-MM-DD")
+            cy.get('[data-cy=end-date-select]>[data-cy=date-select]').should("have.value", today)
 
-    })
+        })
+        it("Check the report if its exists and is visible", () => {
+            cy.get("[data-cy=report-table]").should("not.exist")
+
+        })
 })
    
 
